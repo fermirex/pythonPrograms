5173c5b130507d0c504dc9b411fe517d4d01f954
Ren Shiming <fermirex@outlook.com>
2021-03-10 08:29:43 +0800

    multipal gid process file

---
+++ multiple_gid.py
@@ -0,0 +1,20 @@
+import re
+def handle_g_Tag(matched):
+    print(matched.groupdict())
+    try:
+        int(matched.groupdict()["g_Tag_Num"])
+        lineString_new = '<g' + matched.groupdict()["g_Tag_Num"] + ">" + matched.groupdict()["g_Tag_content"] + "g" + \
+                     matched.groupdict()["g_Tag_Num"] + ">"
+    except:
+        # raise ValueError("the g id include non-number chars")
+        lineString_new = "<g_id_include_non_number_char(s)>"
+    finally:
+        return lineString_new
+
+lineString = 'V<g id="2160"><g id="2161">G</g></g>/V<g id="2162"><g id="2163">L</g></g> is also called the phase ratio'
+print(lineString)
+lineString_new = re.sub(
+    r'(?P<g_Tag_Start><g id=")(?P<g_Tag_Num>.*?)(?P<g_Tag_NumEnd>">)(?P<g_Tag_content>.*?</)(?P<g_Tag_End>g>)',
+    handle_g_Tag, lineString)
+
+print(lineString_new)
--- reSample.py
+++ reSample.py
@@ -29,17 +29,24 @@ import re
 
 def handle_g_Tag(matched):
     print(matched.groupdict())
-    lineString = '<g' + matched.groupdict()["g_Tag_Num"] + ">" + matched.groupdict()["g_Tag_content"] + "g" + matched.groupdict()["g_Tag_Num"] + ">"
-    return lineString
+    try:
+        int(matched.groupdict()["g_Tag_Num"])
+        lineString_new = '<g' + matched.groupdict()["g_Tag_Num"] + ">" + matched.groupdict()["g_Tag_content"] + "g" + \
+                     matched.groupdict()["g_Tag_Num"] + ">"
+    except:
+        # raise ValueError("the g id include non-number chars")
+        lineString_new = "<g_id_include_non_number_char(s)>"
+    finally:
+        return lineString_new
 
 # with open("filepath", mode="a+", encoding="UTF8") as f:
 #     while True:
 #         lineString = f.readline()
 
-lineString = '# Click <g id="90">OK</g> to acknowledge the warning <g id="100">OK</g> message.'
+lineString = '# Click <g id="90">OK</g> to acknowledge the warning <g id="10o">OK</g> message.'
 print(lineString)
-lineString = re.sub(r'(?P<g_Tag_Start><g id=")(?P<g_Tag_Num>\d+)(?P<g_Tag_NumEnd>">)(?P<g_Tag_content>.*?</)(?P<g_Tag_End>g>)',handle_g_Tag,lineString)
+lineString_new = re.sub(r'(?P<g_Tag_Start><g id=")(?P<g_Tag_Num>.*?)(?P<g_Tag_NumEnd>">)(?P<g_Tag_content>.*?</)(?P<g_Tag_End>g>)',handle_g_Tag,lineString)
 
-print(lineString)
+print(lineString_new)
 # Click <g id="90">OK</g> to acknowledge the warning message.
 # Click <g90>OK</g90> to acknowledge the warning message.
